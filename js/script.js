bfb3f0ad10629d6f7564e305a05f21877092ba77
asep komarudin <pojokcodeid@gmail.com>
2023-05-08 17:52:55 +0700

    update

--- js/script.js
+++ js/script.js
@@ -69,3 +69,58 @@
       })
   })
 })()
+
+// custom sidebar
+function openNav() {
+  let sidebar=document.getElementById("mySidebar");
+  let main=document.getElementById("main");
+  sidebar.style.width = "250px";
+  main.style.marginLeft = "250px";
+}
+
+function closeNav() {
+  let sidebar=document.getElementById("mySidebar");
+  let main=document.getElementById("main");
+  let iconbar=document.getElementById("sidebarCollapse");
+  if(sidebar.hasAttribute('inactive')){
+    sidebar.style.width = "300px";
+    sidebar.classList.add("active");
+    sidebar.classList.add("col-2");
+    sidebar.classList.remove("active2");
+    sidebar.removeAttribute('inactive');
+    main.classList.add("col-10");
+    main.classList.remove("col-12");
+    main.style.marginLeft= "300px";
+    main.style.transition= "all 0.3s";
+  }else{
+    sidebar.style.width = "0";
+    sidebar.setAttribute('inactive', 'inactive');
+    sidebar.classList.add("active2");
+    sidebar.classList.remove("col-2");
+    main.classList.add("col-12");
+    main.classList.remove("col-10");
+    main.style.marginLeft= "0";
+    main.style.transition= "all 0.3s";
+  }
+  console.log(iconbar.hasAttribute('inactive'));
+  if(iconbar.hasAttribute('inactive')){   
+    let iconbar=document.getElementById("sidebarCollapse");
+    let btnClose=document.getElementById("btnClose");
+    let icon=document.createElement("i");
+    icon.classList.add("fas");
+    icon.classList.add("fa-bars");
+    icon.setAttribute("id","btnBar");
+    iconbar.appendChild(icon);
+    iconbar.removeChild(btnClose);
+    iconbar.removeAttribute('inactive');
+  }else{
+    iconbar.setAttribute('inactive', '');
+    let btnBar=document.getElementById("btnBar");
+    iconbar.removeChild(btnBar);
+    let icon=document.createElement("i");
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-xmark");
+    icon.setAttribute("id","btnClose");
+    iconbar.appendChild(icon);
+  }
+}
