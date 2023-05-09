8c11d9666764228aaa8ee69a48ca28d7b5293d7c
asep komarudin <pojokcodeid@gmail.com>
2023-05-09 13:03:33 +0700

    update

--- js/script.js
+++ js/script.js
@@ -82,24 +82,33 @@ function closeNav() {
   let sidebar=document.getElementById("mySidebar");
   let main=document.getElementById("main");
   let iconbar=document.getElementById("sidebarCollapse");
+  var x = window.matchMedia("(max-width: 990px)")
   if(sidebar.hasAttribute('inactive')){
-    sidebar.style.width = "300px";
-    sidebar.classList.add("active");
-    sidebar.classList.add("col-2");
-    sidebar.classList.remove("active2");
-    sidebar.removeAttribute('inactive');
-    main.classList.add("col-10");
-    main.classList.remove("col-12");
-    main.style.marginLeft= "300px";
     main.style.transition= "all 0.3s";
+    main.classList.remove("col-12");
+    if(x.matches){
+      main.classList.add("col-8");
+      sidebar.classList.add("col-4");
+    }else{
+      main.classList.add("col-10");
+      sidebar.classList.add("col-2");
+    }
+    sidebar.style.display='';
+    sidebar.classList.add("active");
+    sidebar.style.marginLeft="0";
+    sidebar.removeAttribute('inactive');
+    sidebar.style.transition= "all 0.3s";
   }else{
-    sidebar.style.width = "0";
     sidebar.setAttribute('inactive', 'inactive');
-    sidebar.classList.add("active2");
-    sidebar.classList.remove("col-2");
+    sidebar.style.transition= "all 0.3s";
+    if(x.matches){
+      sidebar.style.marginLeft="-33.34%";
+      main.classList.remove("col-8");
+    }else{
+      sidebar.style.marginLeft="-16.67%";
+      main.classList.remove("col-10");
+    }
     main.classList.add("col-12");
-    main.classList.remove("col-10");
-    main.style.marginLeft= "0";
     main.style.transition= "all 0.3s";
   }
   console.log(iconbar.hasAttribute('inactive'));
@@ -124,3 +133,53 @@ function closeNav() {
     iconbar.appendChild(icon);
   }
 }
+
+// for responsive screen
+function myFunction(x) {
+  let sidebar=document.getElementById("mySidebar");
+  let main=document.getElementById("main");
+  let iconbar=document.getElementById("sidebarCollapse");
+  if (x.matches) { // If media query matches
+    // ini untuk screen
+    sidebar.setAttribute('inactive', 'inactive');
+    sidebar.style.marginLeft="-16.67%";
+    sidebar.style.transition= "all 0.3s";
+    main.classList.add("col-12");
+    main.classList.remove("col-10");
+    main.style.transition= "all 0.3s";
+    // ini untuk button
+    let iconbar=document.getElementById("sidebarCollapse");
+    let btnClose=document.getElementById("btnClose");
+    let icon=document.createElement("i");
+    icon.classList.add("fas");
+    icon.classList.add("fa-bars");
+    icon.setAttribute("id","btnBar");
+    iconbar.appendChild(icon);
+    iconbar.removeChild(btnClose);
+    iconbar.removeAttribute('inactive');
+  } else {
+    //ini untuk screen
+    main.style.transition= "all 0.3s";
+    main.classList.add("col-10");
+    main.classList.remove("col-12");
+    sidebar.classList.add("active");
+    sidebar.classList.remove("col-4");
+    sidebar.classList.add("col-2");
+    sidebar.style.marginLeft="0";
+    sidebar.removeAttribute('inactive');
+    sidebar.style.transition= "all 0.3s";
+    // ini untuk button
+    iconbar.setAttribute('inactive', '');
+    let btnBar=document.getElementById("btnBar");
+    iconbar.removeChild(btnBar);
+    let icon=document.createElement("i");
+    icon.classList.add("fa-solid");
+    icon.classList.add("fa-xmark");
+    icon.setAttribute("id","btnClose");
+    iconbar.appendChild(icon);
+  }
+}
+
+var x = window.matchMedia("(max-width: 990px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
